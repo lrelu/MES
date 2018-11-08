247c587fcc271a7344d4112c56d24ec9e7e56248
lrelu <newlik@naver.com>
2018-11-08 15:39:59 +0900

    : Feat - Circular Progressed bar and safe thread
    using nuget

--- Source_MES/Dump/Form1.Designer.cs
+++ Source_MES/Dump/Form1.Designer.cs
@@ -28,13 +28,69 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// circularProgressBar1
+			// 
+			this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+			this.circularProgressBar1.AnimationSpeed = 500;
+			this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+			this.circularProgressBar1.Font = new System.Drawing.Font("굴림", 72F, System.Drawing.FontStyle.Bold);
+			this.circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.circularProgressBar1.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.circularProgressBar1.InnerMargin = 2;
+			this.circularProgressBar1.InnerWidth = -1;
+			this.circularProgressBar1.Location = new System.Drawing.Point(12, 12);
+			this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+			this.circularProgressBar1.Name = "circularProgressBar1";
+			this.circularProgressBar1.OuterColor = System.Drawing.Color.Gray;
+			this.circularProgressBar1.OuterMargin = -25;
+			this.circularProgressBar1.OuterWidth = 26;
+			this.circularProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.circularProgressBar1.ProgressWidth = 25;
+			this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("굴림", 36F);
+			this.circularProgressBar1.Size = new System.Drawing.Size(320, 320);
+			this.circularProgressBar1.StartAngle = 270;
+			this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+			this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+			this.circularProgressBar1.SubscriptText = ".23";
+			this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+			this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+			this.circularProgressBar1.SuperscriptText = "°C";
+			this.circularProgressBar1.TabIndex = 0;
+			this.circularProgressBar1.Text = "0";
+			this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+			this.circularProgressBar1.Value = 68;
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point(395, 29);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(113, 83);
+			this.btnStart.TabIndex = 1;
+			this.btnStart.Text = "Start";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(565, 389);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.circularProgressBar1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private CircularProgressBar.CircularProgressBar circularProgressBar1;
+		private System.Windows.Forms.Button btnStart;
 	}
 }
 
--- Source_MES/Dump/Form1.cs
+++ Source_MES/Dump/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,62 @@ namespace Dump
 {
 	public partial class Form1 : Form
 	{
+		private Random _rand = new Random();
+		private delegate void ChangeCircularPBValueDelegate(int value);
+
 		public Form1()
 		{
 			InitializeComponent();
+			this.circularProgressBar1.Minimum = 0;
+			this.circularProgressBar1.Maximum = 100;
+			this.circularProgressBar1.Value = 70;
+			this.circularProgressBar1.Text = "70";
+
+			this.circularProgressBar1.Update();
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			// 스레드 사용 기본
+			Thread thread = new Thread(new ThreadStart(Run));
+			thread.Start();
+		}
+
+		private void SetValue(int value)
+		{
+			if (this.circularProgressBar1.InvokeRequired)
+			{
+				ChangeCircularPBValueDelegate run = new ChangeCircularPBValueDelegate(SetValue);
+				this.Invoke(run, new object[] { value });
+			}
+			else
+			{
+				this.circularProgressBar1.Value = value;
+				this.circularProgressBar1.Text = value.ToString();
+				this.circularProgressBar1.Update();
+			}
+		}
+
+		private void Run()
+		{
+			//외부 스레드에서 컨트롤을 수정하기 때문에 디버그시 오류가 발생함
+			//while (true)
+			//{
+			//	int temp = this.circularProgressBar1.Value + _rand.Next(-5, 5);
+			//	this.circularProgressBar1.Value = temp;
+			//	this.circularProgressBar1.Text = temp.ToString();
+			//	this.circularProgressBar1.Update();
+			//	Thread.Sleep(100);
+			//}
+
+			// invoke required 방식으로 변경
+			while (true)
+			{
+				int temp = this.circularProgressBar1.Value + _rand.Next(-5, 5);
+				this.SetValue(temp);
+				Thread.Sleep(100);
+			}
+			
 		}
 	}
 }
