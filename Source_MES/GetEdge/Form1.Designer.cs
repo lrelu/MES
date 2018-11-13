94d848a57145883585108b0375472f9c9d5c0a4e
lrelu <newlik@naver.com>
2018-11-13 09:49:35 +0900

    : feat - Get Edge line from Image
    Mask and get out edge

---
+++ Source_MES/GetEdge/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace GetEdge
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.btnLoad = new System.Windows.Forms.Button();
+			this.btnGetEdge = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.btnGetEdge);
+			this.panel1.Controls.Add(this.btnLoad);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(78, 512);
+			this.panel1.TabIndex = 0;
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(78, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+			this.splitContainer1.Size = new System.Drawing.Size(1028, 512);
+			this.splitContainer1.SplitterDistance = 512;
+			this.splitContainer1.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(512, 512);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			// 
+			// btnLoad
+			// 
+			this.btnLoad.Location = new System.Drawing.Point(3, 12);
+			this.btnLoad.Name = "btnLoad";
+			this.btnLoad.Size = new System.Drawing.Size(69, 65);
+			this.btnLoad.TabIndex = 0;
+			this.btnLoad.Text = "Load";
+			this.btnLoad.UseVisualStyleBackColor = true;
+			this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+			// 
+			// btnGetEdge
+			// 
+			this.btnGetEdge.Location = new System.Drawing.Point(3, 83);
+			this.btnGetEdge.Name = "btnGetEdge";
+			this.btnGetEdge.Size = new System.Drawing.Size(69, 65);
+			this.btnGetEdge.TabIndex = 0;
+			this.btnGetEdge.Text = "Get Edge";
+			this.btnGetEdge.UseVisualStyleBackColor = true;
+			this.btnGetEdge.Click += new System.EventHandler(this.btnGetEdge_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1106, 512);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button btnGetEdge;
+		private System.Windows.Forms.Button btnLoad;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+	}
+}
+
---
+++ Source_MES/GetEdge/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetEdge
+{
+	public partial class Form1 : Form
+	{
+		System.Byte[,] inImage, outImage;
+		int inH, inW, outH, outW;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void LoadImage(string fileName)
+		{
+			System.Int64 fileSize = new System.IO.FileInfo(fileName).Length;
+			this.inH = this.inW = (int)Math.Sqrt(fileSize);
+
+			this.inImage = new System.Byte[inH, inW];
+
+			using (BinaryReader br = new BinaryReader(File.Open(fileName, FileMode.Open)))
+			{
+				for (int i = 0; i < inH; i++)
+				{
+					for (int k = 0; k < inW; k++)
+					{
+						System.Byte pixel = br.ReadByte();
+						inImage[k, i] = pixel;
+					}
+				}
+			}
+		}
+
+		private void DisplayImage(byte[,] image, Control ctl)
+		{
+			System.Byte pixel;
+			Color color;
+			Bitmap bitmap = new Bitmap(inW, inH);
+
+			for (int i = 0; i < inH; i++)
+			{
+				for (int k = 0; k < inW; k++)
+				{
+					pixel = image[i, k];
+					color = Color.FromArgb(pixel, pixel, pixel);
+					bitmap.SetPixel(i, k, color);
+				}
+			}
+
+			((PictureBox)ctl).Image = bitmap;
+		}
+
+		private void btnLoad_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog dialog = new OpenFileDialog())
+			{
+				dialog.DefaultExt = "raw";
+				dialog.Filter = "RAW File(*.raw) | *.raw";
+				dialog.InitialDirectory = @"C:\Temp\Etc_Raw\512";
+				dialog.RestoreDirectory = true;
+
+				if (dialog.ShowDialog() == DialogResult.OK)
+				{
+					this.LoadImage(dialog.FileName);
+					this.DisplayImage(this.inImage, this.pictureBox1);
+
+				}
+			}
+		}
+
+		private void btnGetEdge_Click(object sender, EventArgs e)
+		{
+			double[,] eboMask = { { -1.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0 }, { 0.0, 0.0, 1.0 } };
+			//double[,] edgeHMask = { { 0.0, -1.0, 0.0 }, { 0.0, 1.0, 0.0 }, { 0.0, 0.0, 1.0 } };
+			//double[,] edgeVMask = { { 0.0, 0.0, 0.0 }, { -1.0, 1.0, 0.0 }, { 0.0, 0.0, 1.0 } };
+
+			System.Byte[,] tempOut = this.MakeFilter(eboMask);
+
+			this.DisplayImage(tempOut, this.pictureBox2);
+		}
+
+		private System.Byte[,] MakeFilter(double[,] mask)
+		{
+			double[,] tempIn = new double[this.inW + 2, this.inH + 2];
+			System.Byte[,] tempOut = new System.Byte[this.inW, this.inH];
+
+			// 원본이미지를 여백이 있는 임시 double 배열에다가 저장
+			for (int i = 0; i < inH; i++)
+				for (int k = 0; k < inW; k++)
+					tempIn[i + 1, k + 1] = (double)this.inImage[i, k];
+
+			for (int i = 0; i < inH; i++)
+			{
+				for (int k = 0; k < inW; k++)
+				{
+					// 마스킹
+					double sum = 0.0;
+					for (int m = -1; m < mask.GetLength(0) - 1; m++)
+						for (int n = -1; n < mask.GetLength(1) - 1; n++)
+							sum += tempIn[(i + 1) + m, (k + 1) + n] * mask[m + 1, n + 1];
+
+					//sum = sum > 255.0 ? 255.0 : sum;
+					//sum = sum < 0.0 ? 0.0 : sum;
+					//tempOut[i, k] = (System.Byte)sum;
+
+					tempOut[i, k] = (System.Byte)(sum + 127);
+				}
+			}
+
+			return tempOut;
+		}
+	}
+}
